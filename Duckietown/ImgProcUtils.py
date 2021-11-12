59f287cbc09ed740484120de4e0a201f58f4a32a
Hamad Ullah <61397521+ullahhab@users.noreply.github.com>
2021-11-12 03:00:33 -0600

    Update ImgProcUtils.py

--- Duckietown/ImgProcUtils.py
+++ Duckietown/ImgProcUtils.py
@@ -2,6 +2,7 @@ from cv2 import cv2
 from scipy import misc
 import numpy as np
 
+#This will always return 0 not sure if you need this line. 
 vertical_limit = 2//3  # This number should be less than 1. It is how much of the the frame is considered.
 
 
@@ -13,22 +14,31 @@ def detect_which_lane(image):
     white_upper_hsv = np.array([150, 100, 255])
     yellow_lower_hsv = np.array([10, 110, 0])     # 10, 110, 0
     yellow_upper_hsv = np.array([45, 255, 255])   # 45, 255, 255
-
+    
+    
     image = image[int(len(image)*vertical_limit):]
-    halfway = int(len(image[0])/2)
+    #You need //(integer division just in case if the number is odd)
+    halfway = int(len(image[0])//2)
     image_left = image[:, :halfway]
     image_right = image[:, halfway:]
-
+    
+    #When you do inrange the return is 2-d array. Picture only picks out colors in range and modify the array/
     mask_white_left = cv2.inRange(image_left, white_lower_hsv, white_upper_hsv)
     mask_yellow_left = cv2.inRange(image_left, yellow_lower_hsv, yellow_upper_hsv)
 
     mask_white_right = cv2.inRange(image_right, white_lower_hsv, white_upper_hsv)
     mask_yellow_right = cv2.inRange(image_right, yellow_lower_hsv, yellow_upper_hsv)
-
+    
+    #Adding these lines for your understanding
+    print(mask_white_right.shape)
+    print(mask_white_left.shape)
+    print(mask_yellow_right.shape)
+    print(mask_yellow_left.shape)
+    
     print("left yellow ", sum(mask_yellow_left))
     # print("left white  ", sum(mask_white_left[0:0, 0]))
-
-    print("right yellow  ", sum(mask_yellow_right[:, :, :]))
+    #you Need less indicies here. Your image just converted to 2-d array
+    print("right yellow  ", sum(mask_yellow_right[:, :]))
     # print("right white   ", sum(mask_white_right[0]))
 
     cv2.imshow("left", mask_yellow_left[:, :])
