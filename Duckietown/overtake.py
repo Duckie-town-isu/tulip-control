744c74cf76e282e142b7b377aaa6dce4da29739c
ranai-srivastav <sweetynranai@gmail.com>
2022-01-02 17:45:11 -0600

    Trying to read from the camera

--- Duckietown/overtake.py
+++ Duckietown/overtake.py
@@ -18,10 +18,17 @@ from cv2 import cv2
 """
 
 # Getting the image from the camera
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    ret, frame = cap.read()
+dispW = 320
+dispH = 240
+flip = 2
+# How to locate the camera through the PCI
+camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+camera = cv2.VideoCapture(camSet)
+
+while camera.isOpened():
+    ret, frame = camera.read()
     if ret:
         key = cv2.waitKey(1)
         if key == ord('q'):
@@ -29,10 +36,15 @@ while cap.isOpened():
         else:
             cv2.imshow("Name", frame)
             # left, right = img.detectLane(frame)
-            cv2.imshow("Detect left lane or right", detect_which_lane(frame))
+            # cv2.imshow("Detect left lane or right",
+            detect_which_lane(frame)
     else:
         break
 
-cap.release()
+camera.release()
 cv2.destroyAllWindows()
+
+
+"""
+dispW=320 dispH=240 flip=2 camSet='nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink' camera = cv2.VideoCapture(camSet): while Ture: ret. frame=cam.read() cv2.imshow("Open_camera_window", frame) if cv2.waitKey(1)==ord('q'): break cam.release() #cv2.destroyAllWindows()"""
 
